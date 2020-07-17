65ff74d236d334e462314d09d7cd10fd8744f27e
Swarnava Basu <basuswarnava2@gmail.com>
2020-07-18 01:19:22 +0530

    add home, cart and auth page

--- server.js
+++ server.js
@@ -9,14 +9,27 @@ const PORT = process.env.PORT || 3300
 app.use(express.static('public'))
 
 
-app.get('/',(req, res) => {
-    res.render('home')
-})
+
 
 //set template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+
+app.get('/',(req, res) => {
+    res.render('home')
+})
+
+app.get('/cart', (req, res)=>{
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res)=>{
+    res.render('auth/login')
+})
+app.get('/register', (req, res)=>{
+    res.render('auth/register')
+})
 
 app.listen(PORT, () =>{
     console.log('Listening on port 3300')
