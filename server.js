ee550214e011b5fb52c3c4eecfb745cb8f564f3b
Swarnava Basu <basuswarnava2@gmail.com>
2020-07-24 00:46:25 +0530

    working on

--- server.js
+++ server.js
@@ -16,20 +16,8 @@ app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-app.get('/',(req, res) => {
-    res.render('home')
-})
+require('./routes/web')(app)
 
-app.get('/cart', (req, res)=>{
-    res.render('customers/cart')
-})
-
-app.get('/login', (req, res)=>{
-    res.render('auth/login')
-})
-app.get('/register', (req, res)=>{
-    res.render('auth/register')
-})
 
 app.listen(PORT, () =>{
     console.log('Listening on port 3300')
