56e0d4eb29c8287dc333e16fcd35f293dc4a7512
Swarnava Basu <basuswarnava2@gmail.com>
2020-07-27 01:23:45 +0530

    Working on

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,17 @@ function updateCart(pizza){
     axios.post('/update-cart', pizza).then(res => {
         cartCounter.innerText = res.data.totalQty
         new Noty ({
-            text:'Item added to cart'
+            type:'success',
+            timeout: 1000,
+            text:'Item added to cart',
+            progressBar: false
+        }).show();
+    }).catch(err => {
+        new Noty ({
+            type:'error',
+            timeout: 1000,
+            text:'Something went wrong',
+            progressBar: false
         }).show();
     })
 }
